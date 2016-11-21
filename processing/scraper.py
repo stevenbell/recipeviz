c1cd28f431105d1f11ab8abda0e821ac14535998
Steven Bell <botsnlinux@gmail.com>
2016-11-21 11:49:53 -0800

    Added category to the scraper

--- processing/scraper.py
+++ processing/scraper.py
@@ -6,6 +6,7 @@ from IPython import embed
 from lxml import html
 import requests
 import time
+import codecs # Write unicode files
 
 # Individual recipes are at
 # http://allrecipes.com/recipe/[NUMBER]/[...]
@@ -14,27 +15,27 @@ import time
 # Not all numbers are valid; there is probably some kind of numbering scheme
 # based on the category.
 
-outfile = open('ingredients.csv', 'w')
-outfile.write('id,name,ingredients\n')
+# Don't overwrite, just append, which we can fix manually later
+outfile = codecs.open('ingredients.csv', encoding='utf-8', mode='w+')
+outfile.write('id,name,category,ingredients\n')
 
-for number in range(9500, 10500):
+for number in range(9500, 12500):
   url = "http://allrecipes.com/recipe/%d" % number
 
   try:
     page = requests.get(url)
 
-    # tmp = open('%d.html' % number, 'w')
-    # tmp.write(page.content)
-    # tmp.close()
-
     document = html.fromstring(page.content)
   
     name = document.xpath('//h1[@class="recipe-summary__h1"]/text()')[0]
-  
+
+    categorybits = document.xpath('//ul[@class="breadcrumbs breadcrumbs"]/li/a/span[@itemprop="title"]/text()')
+    category = '>'.join([c.strip() for c in categorybits])
+
     inglines = document.xpath('//span[@class="recipe-ingred_txt added"]')
     ingredients = [i.xpath('text()')[0] for i in inglines]
   
-    outfile.write('%d,"%s","%s"\n' % (number, name, str(ingredients)))
+    outfile.write('%d,"%s","%s","%s"\n' % (number, name, category, str(ingredients)))
     print name
   
   except Exception as e:
