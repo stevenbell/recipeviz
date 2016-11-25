a237a78b3a660d96c1410223a4e5d4fa2da260fc
Steven Bell <botsnlinux@gmail.com>
2016-11-25 11:02:57 -0800

    Adding more known ingredients.

--- processing/ingredientmap.py
+++ processing/ingredientmap.py
@@ -1,24 +1,48 @@
 
 ingredient_remap = {
+"bread flour":"flour",
+"wheat flour":"flour",
+"lemon peel":"lemon zest",
+"orange peel":"orange zest",
 "sugar":"white sugar",
 "vanilla extract":"vanilla",
 "heavy whipping cream":"whipping cream",
-"pecan halv":"pecan"
+"distilled white vinegar":"vinegar",
+"extra virgin olive oil":"olive oil",
+"rolled oat":"oat",
+"pecan halv":"pecan", # Workaround
+"egg whit":"egg white", # Workaround
+"molass":"molasses", # Workaround
+"unsalted butter":"butter" # Questionable, salt content matters
 }
 
 adjectives = [
+'boneless',
 'chopped',
+'cold',
+'cooked',
+'cubed',
 'creamy',
 'diced',
 'dried',
+'flaked',
+'frozen',
+'grated',
 'green',
 'ground',
+'hot',
+'large',
 'low-fat',
 'minced',
+'packed',
+'peeled',
 'red',
 'real',
 'semi-sweet',
+'sifted',
 'shredded',
-'sliced'
+'sliced',
+'unbaked',
+'warm'
 ]
 
