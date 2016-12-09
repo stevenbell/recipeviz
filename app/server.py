077f5301c4ef47b2f74cde0fe19cc677555fbb9a
John Carlo <jcbuena@stanford.edu>
2016-12-09 13:11:33 -0800

    added app, but dynamic loading has bugs

--- app/server.py
+++ app/server.py
@@ -1,20 +1,27 @@
-from flask import Flask, jsonify, request
+import os, copy
+from flask import Flask, jsonify, request, send_from_directory, make_response
+
 from search import search_recipes
 
+app = Flask(__name__, static_url_path='')
 
-
-app = Flask(__name__)
-debug = False
 
 @app.route("/")
-def hello():
-    return "You know nothing, John Snow."
+def index():
+    return app.make_response(open('assets/index.html').read())
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('assets/js', path)
 
 # --------------------------------------------
 # API
 import json
 
 @app.route('/search/<query>', methods=['GET'])
-def search():
-    with open('./assets/data/ingredients_simplified.json') as ingredients:
-	return json.dumps(search_recipes(query, ingredients))
+def search(query):
+	return json.dumps(search_recipes(query, './assets/data/ingredients_simplified.json'))
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5050))
+	app.run(host='0.0.0.0', port=port, debug=True)
